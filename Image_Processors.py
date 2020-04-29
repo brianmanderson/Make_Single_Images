42c434c4714593e22eeb082f828862e50953f8b1
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2020-04-29 10:47:02 -0500

    going to have to mmerge

---
+++ Image_Processors.py
@@ -0,0 +1,14 @@
+__author__ = 'Brian M Anderson'
+# Created on 4/28/2020
+
+
+class Image_Processor(object):
+
+    def process(self, feature_dictionary):
+        '''
+        This is for getting information on the sitk image for later
+        :param feature_dictionary: a dictionary of things like 'image', 'annotation', etc.
+        :return:
+        '''
+        return feature_dictionary
+
